1ff13c12dcd2418f8f96a67c6277cc334fb514d1
maria.mariana.2009 <maria.mariana.2009@aluno.edu.pi.gov.br>
2026-06-11 18:14:42 +0000

    calculadora.js

---
+++ calculadora.js
@@ -0,0 +1,20 @@
+function somarnumeros(n1,n2){
+    console.log (n1+n2);
+}
+function subtração(a,b) {
+  return a-b; 
+}
+function multiplicar(a,b){
+  return a*b;}
+module.exports=multiplicar
+
+function dividir(a,b){
+  return a/b;
+  module.exports=dividir
+}
+module.exports={
+  soma:soma,
+  subtração:subtração,
+  multiplicação:multiplicação,
+  divisão:divisão,
+}
