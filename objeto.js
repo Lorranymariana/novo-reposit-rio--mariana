fea2c3572e4cbdd6ad02e9bdec9f625301c13c1e
maria.mariana.2009 <maria.mariana.2009@aluno.edu.pi.gov.br>
2026-06-16 19:08:07 +0000

    objeto.js

---
+++ objeto.js
@@ -0,0 +1,18 @@
+const pessoa ={
+    nome: "fabio",
+    idade: 17,
+    peso: 50,
+    altura: 1.70,
+    cor: "branco",
+}
+console.log (pessoa.nome);
+
+pessoa.idade=17;
+
+console.log(pessoa.idade);
+
+console.log (pessoa.peso);
+
+console. log (pessoa.altura);
+ 
+console.log (pessoa.cor);
