1dcc5f66e8eb4ff7ba56c855ce69a956d1b11852
maria.mariana.2009 <maria.mariana.2009@aluno.edu.pi.gov.br>
2026-06-18 17:22:39 +0000

    produtos.js

---
+++ funcoes.js
@@ -0,0 +1,7 @@
+const calculadora={
+    numero 1:10,
+    numero 2:5,
+    multiplicar: () {
+     return this.numero 1* this.numero 2}   
+}
+console.log (calculadora.multiplicar()) ;
---
+++ produtos.js
@@ -0,0 +1,13 @@
+const produtos={
+    nome:"tablet",
+    preco:1300,
+    marca: "samsung"};
+
+    for(let chave in produtos){
+    console.log (` ${chave}: ${produtos[chave]}`);
+
+    }
+console.log(Object.keys(produtos));
+console.log(Object.values(produtos));
+
+
